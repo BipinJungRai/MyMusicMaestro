1dd5d81360300961a899bf45a93a182bef5cd35f
Bipin Rai <133552666+BipinJungRai@users.noreply.github.com>
2023-12-03 20:56:37 +0000

    models.py successfully made. seed.py issues still being resolved (password is not being hashed)

---
+++ app_album_viewer/management/commands/seed.py
@@ -0,0 +1,73 @@
+import json
+from pathlib import Path
+from app_album_viewer.models import Album, Song, User, Comment
+from django.core.management import BaseCommand
+from django.core.files.images import ImageFile
+
+ROOT_DIR = Path('app_album_viewer') / 'management'
+
+class Command(BaseCommand):
+    help = 'Seeds the database with initial data'
+
+    def handle(self, *args, **options):
+
+        # Clear existing data
+        Album.objects.all().delete()
+        Song.objects.all().delete()
+        User.objects.all().delete()
+        Comment.objects.all().delete()
+
+        with open(ROOT_DIR / 'sample_data.json') as json_file:
+            sample_data = json.load(json_file)
+            for album_data in sample_data['albums']:
+
+                # Create user
+                user, created = User.objects.get_or_create(
+                    display_name=album_data['comments'][0]['user__display_name'],
+                    defaults={'password': 'password'}
+                )
+
+                # Set the password
+                user.password = 'password'
+
+                # Save the user
+                user.save()
+
+                # Cover art handling
+                image_path = album_data['cover']
+                if image_path is not None:
+                    cover_art_path = Path('app_album_viewer/media/Album Covers') / image_path
+                    kwargs = {'cover_art': ImageFile(open(cover_art_path, 'rb'), name=image_path)}
+                else:
+                    kwargs = {}
+
+                # Create album
+                album = Album.objects.create(
+                    artist=album_data['artist'],
+                    price=album_data['price'],
+                    format=album_data['format'],
+                    release_date=album_data['release_date'],
+                    **kwargs
+                )
+
+                # Create songs and associate them with the album
+                for song_data in sample_data['songs']:
+                    if album_data['title'] in song_data['albums']:
+                        song, created = Song.objects.get_or_create(
+                            title=song_data['title'],
+                            running_time=song_data['runtime']
+                        )
+                        album.songs.add(song)
+
+                # Create comments and associate them with the user and album
+                for comment_data in album_data['comments']:
+                    comment = Comment.objects.create(
+                        user=user,
+                        album=album,
+                        text=comment_data['message']
+                    )
+                    user.album_comments.add(comment)
+                    album.comments.add(comment)
+
+                # Save the album
+                album.save()
--- app_album_viewer/models.py
+++ app_album_viewer/models.py
@@ -1,3 +1,79 @@
+from django.contrib.auth.models import AbstractUser, Permission, Group
 from django.db import models
 
+
 # Create your models here.
+
+class Album(models.Model):
+    # cover art image optional; they use a default image if a cover art image is not specified
+    cover_art = models.ImageField(upload_to='album_covers', null=True, blank=True, default='default_cover.jpg')
+
+    # title, which is required and may not be unique
+    title = models.CharField(max_length=100, unique=False, null=False, blank=False)
+
+    # description, which could be empty
+    description = models.TextField(null=True, blank=True)
+
+    # artist, which is a string, and required
+    artist = models.CharField(max_length=100, null=False, blank=False)
+
+    # price, in GBP, which is required but may be zero
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0.00)
+
+    # format, one of 'Digital download', 'CD', 'Vinyl'
+    FORMAT_CHOICES = [
+        ('Digital download', 'Digital download'),  # 'Digital download' will be stored in the database
+        ('CD', 'CD'),  # 'CD' will be stored in the database
+        ('Vinyl', 'Vinyl'),  # 'Vinyl' will be stored in the database
+    ]
+    format = models.CharField(max_length=20, choices=FORMAT_CHOICES, null=False, blank=False)
+
+    # release date; unreleased albums may have a release date up to three years in the future
+    # (if more than one year away, it must be set to January)
+    release_date = models.DateField(null=False, blank=False)
+
+    # songs (assuming I have a Song model defined separately)
+    songs = models.ManyToManyField('Song', related_name='album_songs')
+
+    # comments (assuming I have a Comment model defined separately)
+    comments = models.ManyToManyField('Comment', related_name='album_comments')
+
+
+class Song(models.Model):
+    # title
+    title = models.CharField(max_length=100, unique=False, null=False, blank=False)
+
+    # running time in seconds
+    running_time = models.IntegerField(null=False, blank=False)
+
+    # albums (assuming I have an Album model defined separately)
+    albums = models.ManyToManyField('Album', related_name='song_albums')
+
+
+class User(AbstractUser):
+    # username
+    username = models.CharField(max_length=50, null=True, blank=True)
+
+    # password
+    password = models.CharField(max_length=50, null=False, blank=False)
+
+    # display name
+    display_name = models.CharField(max_length=100, null=True, blank=True)
+
+    # comments on albums (assuming I have a Comment model defined separately)
+    album_comments = models.ManyToManyField('Comment', related_name='user_comments')
+
+    # permissions
+    user_permissions = models.ManyToManyField(Permission, related_name='user_permissions')
+    groups = models.ManyToManyField(Group, related_name='user_groups')
+
+
+class Comment(models.Model):
+    # user (assuming I have a User model defined separately)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    # album (assuming I have an Album model defined separately)
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+    # comment text
+    text = models.TextField(null=False, blank=False)
